58f6ad2b704117e895a9fbb0fc5fcec96848dc84
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-10 22:10:58 +0100

    add heading for project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,12 @@ import "../styles/Projects.css";
 export default function Projects() {
     return(
         <section id="projects-section">
-            <div id="projects-div">
+            <div className="container" id="projects-div">
                 <h2>Things I've Built</h2>
+                <div id="project1" className="projects"></div>
+                <div id="project2" className="projects"></div>
+                <div id="project3" className="projects"></div>
+                <div id="project4" className="projects"></div>
             </div>
         </section>
     )
