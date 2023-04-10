a74818e7658f2306c99288e8871cd32cd3b89479
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-10 21:49:35 +0100

    remove redundant styles and add boilerplate for project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export default function App() {
     return(
         <main>
             <Hero />
+            <Projects />
         </main>
     )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import "../styles/Projects.css";
+
+export default function Projects() {
+    return(
+        <section id="projects-section">
+            <div id="projects-div">
+                <h2>Things I've Built</h2>
+            </div>
+        </section>
+    )
+}
