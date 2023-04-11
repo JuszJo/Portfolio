7c523a10853b01c1201bd7e7adb325a3c505d13c
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-11 13:07:07 +0100

    add project section component to display projects reducing repeated code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,46 @@
 import "../styles/Projects.css";
 
+function Template1(props) {
+    <div id="project1" className="projects">
+        <img src="" alt="" />
+        <div className="project-text-section">
+            <h3></h3>
+            <p></p>
+            <p className="links"></p>
+            <div className="source">
+                <div className="github"></div>
+                <div className="live"></div>
+            </div>
+        </div>
+    </div>
+}
+
+function Template2(props) {
+    <div id="project2" className="projects">
+        <img src="" alt="" />
+        <div className="project-text-section">
+            <h3></h3>
+            <p></p>
+            <p className="links"></p>
+            <div className="source">
+                <div className="github"></div>
+                <div className="live"></div>
+            </div>
+        </div>
+    </div>
+}
+
 export default function Projects() {
-    return(
+    return (
         <section id="projects-section">
             <div className="container" id="projects-div">
                 <h2>Things I've Built</h2>
-                <div id="project1" className="projects"></div>
-                <div id="project2" className="projects"></div>
-                <div id="project3" className="projects"></div>
-                <div id="project4" className="projects"></div>
+                <div id="project-list">
+                    <Template1 />
+                    <Template2 />
+                    <Template1 />
+                    <Template2 />
+                </div>
             </div>
         </section>
     )
