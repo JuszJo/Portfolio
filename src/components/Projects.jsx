fd7c567265fa9e1897838e1520c7c1a4367245f2
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-11 13:45:18 +0100

    make project list dynamic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,33 +1,79 @@
 import "../styles/Projects.css";
 
-function Template1(props) {
-    <div id="project1" className="projects">
-        <img src="" alt="" />
-        <div className="project-text-section">
-            <h3></h3>
-            <p></p>
-            <p className="links"></p>
-            <div className="source">
-                <div className="github"></div>
-                <div className="live"></div>
+const project = [
+    {
+        index: 1,
+        image: undefined,
+        title: "Space Tour",
+        description: "",
+        stack: ["React", "HTML&CSS"]
+    },
+
+    {
+        index: 2,
+        image: undefined,
+        title: "Product Page",
+        description: "",
+        stack: ["React" , "HTML&CSS"]
+    },
+
+    {
+        index: 3,
+        image: undefined,
+        title: "Pathfinding Visualizer",
+        description: "",
+        stack: ["Vanilla Javascript", "HTML&CSS"]
+    },
+
+    {
+        index: 4,
+        image: undefined,
+        title: "Word of Web Clone",
+        description: "",
+        stack: ["HTML&CSS"]
+    },
+]
+
+function Template1({ details }) {
+    return (
+        <div id="project1" className="projects">
+            <img src="" alt="" />
+            <div className="project-text-section">
+                <h3 className="project-title">{details.title}</h3>
+                <p className="project-description">{details.description}</p>
+                <div className="stack-div">
+                    {details.stack.map((value, index) => {
+                        return <span key={index} className="links stack">{value}</span>
+                    })}
+                </div>
+                <div className="source">
+                    <div className="github"></div>
+                    <div className="live"></div>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
-function Template2(props) {
-    <div id="project2" className="projects">
-        <img src="" alt="" />
-        <div className="project-text-section">
-            <h3></h3>
-            <p></p>
-            <p className="links"></p>
-            <div className="source">
-                <div className="github"></div>
-                <div className="live"></div>
+function Template2({ details }) {
+    return (
+        <div id="project2" className="projects">
+            <img src="" alt="" />
+            <div className="project-text-section">
+                <h3 className="project-title">{details.title}</h3>
+                <p className="project-description">{details.description}</p>
+                <div className="stack-div">
+                    {details.stack.map((value, index) => {
+                        return <span key={details.index} className="links stack">{value}</span>
+                    })}
+                </div>
+                <div className="source">
+                    <div className="github"></div>
+                    <div className="live"></div>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 export default function Projects() {
@@ -36,10 +82,10 @@ export default function Projects() {
             <div className="container" id="projects-div">
                 <h2>Things I've Built</h2>
                 <div id="project-list">
-                    <Template1 />
-                    <Template2 />
-                    <Template1 />
-                    <Template2 />
+                    <Template1 details={project[0]} />
+                    <Template2 details={project[1]} />
+                    <Template1 details={project[2]} />
+                    <Template2 details={project[3]} />
                 </div>
             </div>
         </section>
