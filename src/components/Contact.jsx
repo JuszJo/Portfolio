fad1f3b3a337bbfdc8a4c50c2cf2d0b134917282
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-12 14:37:18 +0100

    add new icons for contact section and style said section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,14 @@
 import "../styles/Contact.css";
 
 import message from "../assets/message.png";
+import linkedin from "../assets/linkedin.svg";
+import twitter from "../assets/twitter.svg";
+import github from "../assets/github.png";
 
 export default function Contact() {
     return(
         <section id="contact-section">
-            <div id="contact-div">
+            <div id="contact-div" className="container">
                 <h2>Say Hello</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -16,6 +19,20 @@ export default function Contact() {
                     <img src={message} alt=""/>
                     <a>Message</a>
                 </button>
+                <div id="socials-div">
+                    <div className="socials">
+                        <img src={linkedin} alt="" />
+                        <a href="">LinkedIn</a>
+                    </div>
+                    <div className="socials">
+                        <img src={twitter} alt="" />
+                        <a href="">Twitter</a>
+                    </div>
+                    <div className="socials">
+                        <img id="footer-github-image" src={github} alt="" />
+                        <a href="">Github</a>
+                    </div>
+                </div>
             </div>
         </section>
     )
