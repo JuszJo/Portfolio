158b9240fa363a38a468cdf3d2a1a506059aa701
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-15 20:41:45 +0100

    add nav button for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,9 @@ export default function Navbar() {
                         <li className="links"><a href="#projects-section">Project</a></li>
                         <li className="links"><a href="#contact-section">Contact</a></li>
                     </ul>
+                    <div id="menu-wrap">
+                        <div id="menu"></div>
+                    </div>
                 </nav>
             </div>
         </header>
